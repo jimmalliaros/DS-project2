0ae96abf490610b97011ccfb722e4625a29ed24b
Jimmaliaros <jimmaliaros@gmail.com>
2025-01-23 18:01:14 +0200

    implemented function swap

--- src/MaxPQ.java
+++ src/MaxPQ.java
@@ -13,7 +13,7 @@ public class MaxPQ implements  PQInterface{
     }
 
 
-
+    
     private  void resize(){
     }
 
@@ -25,8 +25,14 @@ public class MaxPQ implements  PQInterface{
         return size;
     }
 
-    public void insert(Processor){
+    public void insert(Processor x){//Inserts the Processor x in the heap
+            if(isEmpty()){
+                resize();
+            }//Checks if there is available space ,if not it resizes the heap
 
+            heap[++size]=x;//inserts the element in the heap
+
+            swim(size);//lets the newly added element swim
     }
 
     public Processor max(){
@@ -36,4 +42,27 @@ public class MaxPQ implements  PQInterface{
     public Processor getmax(){
 
     }
+
+    //function that helps swim the items to the top
+    private void swim(int i){
+        if(i==1){
+            return;
+        }//if i is root then do nothing
+
+        int parent=i/2;//finds the parent of the element
+
+        //compare parent child with parent i
+        while(i!=1 && heap[i].compareTo(heap[parent])){
+
+        }
+    }
+
+    private void swap(int i,int j){
+        Processor temp=heap[i];
+        heap[i]=heap[j];
+        heap[j]=temp;
+    }//function that helps swap two elements
+
+
+
 }
